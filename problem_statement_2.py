ba308784dd6108472ff94a7d0fd8b261f82b20f6
vinay <vinayraj33@live.com>
2018-06-12 18:12:34 +0530

    removing initial values

--- problem_statement_2.py
+++ problem_statement_2.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
 
+'''
+    temp_max : numpy array consisting all the max temperatures
+    temp_min : numpy array consisting all the min temperatures
+    months   : numpy array range from 0 to 11
+'''
 temp_max = np.array([39, 41, 43, 47, 49, 51, 45, 38, 37, 29, 27, 25])
 temp_min = np.array([21, 23, 27, 28, 32, 35, 31, 28, 21, 19, 17, 18])
 months = np.arange(12)
@@ -12,9 +17,9 @@ def yearly_temps(times, avg, amplitude, time_offset):
 
 
 res_max, cov_max = optimize.curve_fit(yearly_temps, months,
-                                      temp_max, [20, 10, 0])
+                                      temp_max)
 res_min, cov_min = optimize.curve_fit(yearly_temps, months,
-                                      temp_min, [-40, 20, 0])
+                                      temp_min)
 
 days = np.linspace(0, 12, num=365)
 
